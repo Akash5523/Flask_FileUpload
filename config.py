9b3154b661e58fd027a8e98953ea1499d997c36c
Akash Singh <44118858+Akash5523@users.noreply.github.com>
2025-10-31 12:00:24 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-import secrets
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or secrets.token_hex(32)
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
-    MAX_CONTENT_LENGTH = 1024 * 1024 * 1024  # 16 MB limit
